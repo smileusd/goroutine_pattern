066415ad8710c8d5e3e2d974d28c3b6a11df53ed
shentao <tao.shen@transwarp.io>
2017-09-02 18:37:57 +0800

    add pool pattern and use example

---
+++ pool/pool.go
@@ -0,0 +1,79 @@
+package pool
+
+import (
+    "sync"
+    "io"
+    "fmt"
+    "log"
+)
+
+type Pool struct {
+    m *sync.RWMutex
+    resources chan io.Closer
+    closed bool
+    factory func()(io.Closer, error)
+}
+
+func New(fn func()(io.Closer, error), size uint) (*Pool, error) {
+    if size <= 0 {
+        return nil, fmt.Errorf("pool size too small")
+    }
+    return &Pool{
+        m: &sync.RWMutex{},
+        resources: make(chan io.Closer, size),
+        closed: true,
+        factory: fn,
+    }, nil
+}
+
+func (p *Pool) Acquire() (io.Closer, error) {
+    p.m.RLock()
+    p.m.RUnlock()
+
+    select{
+    case r, ok := <-p.resources:
+        if !ok {
+            return nil, fmt.Errorf("pool has been closed")
+        }
+        return r, nil
+
+    default:
+        return p.factory()
+    }
+}
+
+func (p *Pool) Release(r io.Closer) {
+    p.m.Lock()
+    defer p.m.Unlock()
+
+    if p.closed {
+        r.Close()
+        return
+    }
+
+    select {
+    case p.resources <- r:
+        log.Println("release resource: ", r)
+
+    default:
+        log.Println("closing resource")
+        r.Close()
+    }
+}
+
+func (p *Pool) Close() {
+    p.m.Lock()
+    defer p.m.Unlock()
+
+    if p.closed {
+        return
+    }
+
+    p.closed = true
+
+    close(p.resources)
+
+    for r := range p.resources {
+        r.Close()
+    }
+}
