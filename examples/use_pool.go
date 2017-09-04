540574fa1157651e53ad8acab52d12e8fec681a9
shentao <tao.shen@transwarp.io>
2017-09-04 10:46:46 +0800

    gofmt

--- examples/use_pool.go
+++ examples/use_pool.go
@@ -1,76 +1,76 @@
 package main
 
 import (
-    "log"
-    "sync/atomic"
-    "fmt"
-    "time"
-    "math/rand"
-    "sync"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
 
-    "../pool"
-    "io"
+	"../pool"
 )
 
-const(
-    maxGoroutineNum         = 20
-    connectionPoolSize uint = 2
+const (
+	maxGoroutineNum         = 20
+	connectionPoolSize uint = 2
 )
 
 var idCounter int32
 
 type dbConnection struct {
-    ID int32
+	ID int32
 }
 
 func (c *dbConnection) Close() error {
-    log.Println("close db connection: ", c.ID)
-    return nil
+	log.Println("close db connection: ", c.ID)
+	return nil
 }
 
 func createDbConnection() (io.Closer, error) {
-    id := atomic.AddInt32(&idCounter, 1)
-    log.Println("create db connection: ", id)
-    return &dbConnection{
-        ID: id,
-    }, nil
+	id := atomic.AddInt32(&idCounter, 1)
+	log.Println("create db connection: ", id)
+	return &dbConnection{
+		ID: id,
+	}, nil
 }
 
 func performQuries(query int, p *pool.Pool) error {
-    if p == nil {
-        return fmt.Errorf("pool is nil")
-    }
-    conn, err := p.Acquire()
-    if err != nil {
-        return err
-    }
+	if p == nil {
+		return fmt.Errorf("pool is nil")
+	}
+	conn, err := p.Acquire()
+	if err != nil {
+		return err
+	}
 
-    defer p.Release(conn)
+	defer p.Release(conn)
 
-    time.Sleep(time.Duration(rand.Intn(1000))*time.Millisecond)
-    log.Printf("QID[%d] CID[%d]\n", query, conn.(*dbConnection).ID)
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	log.Printf("QID[%d] CID[%d]\n", query, conn.(*dbConnection).ID)
 
-    return nil
+	return nil
 }
 
 func main() {
-    var wg sync.WaitGroup
-    wg.Add(maxGoroutineNum)
-    connPool, err := pool.New(createDbConnection, connectionPoolSize)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    defer connPool.Close()
+	var wg sync.WaitGroup
+	wg.Add(maxGoroutineNum)
+	connPool, err := pool.New(createDbConnection, connectionPoolSize)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer connPool.Close()
 
-    for query := 0; query < maxGoroutineNum; query ++ {
-        go func() {
-            if err := performQuries(query, connPool); err != nil {
-                log.Println(err)
-            }
-            wg.Done()
-        }()
-    }
-    wg.Wait()
-    log.Println("shutdone the program")
+	for query := 0; query < maxGoroutineNum; query++ {
+		go func() {
+			if err := performQuries(query, connPool); err != nil {
+				log.Println(err)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	log.Println("shutdone the program")
 }
--- pool/pool.go
+++ pool/pool.go
@@ -1,79 +1,79 @@
 package pool
 
 import (
-    "sync"
-    "io"
-    "fmt"
-    "log"
+	"fmt"
+	"io"
+	"log"
+	"sync"
 )
 
 type Pool struct {
-    m *sync.RWMutex
-    resources chan io.Closer
-    closed bool
-    factory func()(io.Closer, error)
+	m         *sync.RWMutex
+	resources chan io.Closer
+	closed    bool
+	factory   func() (io.Closer, error)
 }
 
-func New(fn func()(io.Closer, error), size uint) (*Pool, error) {
-    if size <= 0 {
-        return nil, fmt.Errorf("pool size too small")
-    }
-    return &Pool{
-        m: &sync.RWMutex{},
-        resources: make(chan io.Closer, size),
-        closed: true,
-        factory: fn,
-    }, nil
+func New(fn func() (io.Closer, error), size uint) (*Pool, error) {
+	if size <= 0 {
+		return nil, fmt.Errorf("pool size too small")
+	}
+	return &Pool{
+		m:         &sync.RWMutex{},
+		resources: make(chan io.Closer, size),
+		closed:    true,
+		factory:   fn,
+	}, nil
 }
 
 func (p *Pool) Acquire() (io.Closer, error) {
-    p.m.RLock()
-    p.m.RUnlock()
+	p.m.RLock()
+	p.m.RUnlock()
 
-    select{
-    case r, ok := <-p.resources:
-        if !ok {
-            return nil, fmt.Errorf("pool has been closed")
-        }
-        return r, nil
+	select {
+	case r, ok := <-p.resources:
+		if !ok {
+			return nil, fmt.Errorf("pool has been closed")
+		}
+		return r, nil
 
-    default:
-        return p.factory()
-    }
+	default:
+		return p.factory()
+	}
 }
 
 func (p *Pool) Release(r io.Closer) {
-    p.m.Lock()
-    defer p.m.Unlock()
+	p.m.Lock()
+	defer p.m.Unlock()
 
-    if p.closed {
-        r.Close()
-        return
-    }
+	if p.closed {
+		r.Close()
+		return
+	}
 
-    select {
-    case p.resources <- r:
-        log.Println("release resource: ", r)
+	select {
+	case p.resources <- r:
+		log.Println("release resource: ", r)
 
-    default:
-        log.Println("closing resource")
-        r.Close()
-    }
+	default:
+		log.Println("closing resource")
+		r.Close()
+	}
 }
 
 func (p *Pool) Close() {
-    p.m.Lock()
-    defer p.m.Unlock()
+	p.m.Lock()
+	defer p.m.Unlock()
 
-    if p.closed {
-        return
-    }
+	if p.closed {
+		return
+	}
 
-    p.closed = true
+	p.closed = true
 
-    close(p.resources)
+	close(p.resources)
 
-    for r := range p.resources {
-        r.Close()
-    }
+	for r := range p.resources {
+		r.Close()
+	}
 }
